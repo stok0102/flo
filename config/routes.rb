78d703293d1a0c15d82a9787bf82d1bb920a1074
Caleb Stokka and Tony Fabery <caleb@>
2016-09-13 11:23:50 -0700

    add question model, passing specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users
+  resources :users do
+    resources :questions
+  end
+
   get '/log-in' => 'sessions#new'
   post "/log-in" => "sessions#create"
   get "/log-out" => "sessions#destroy", as: :log_out
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913151657) do
+ActiveRecord::Schema.define(version: 20160913181457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
